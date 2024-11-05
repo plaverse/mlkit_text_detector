27ba7facb1583789566b0447cc496cea779c2349
plaverse <plaverse.email@gmail.com>
2024-11-05 18:10:43 +0900

    mlkit text recognition

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:part5_mlkit_text_detector_starter/camera_view_page.dart';
-import 'package:part5_mlkit_text_detector_starter/text_detector_painter.dart';
+import 'package:mlkit_text_detector/camera_view_page.dart';
+import 'package:mlkit_text_detector/text_detector_painter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,11 +39,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  File? _image;
+  bool _canProcess = true;
+  bool _isBusy = false;
+  String _text = "";
+  CustomPaint? _customPaint;
+  final _cameraLensDirection = CameraLensDirection.back;
 
+  var _script = TextRecognitionScript.korean;
+  TextRecognizer textRecognizer = TextRecognizer(script: TextRecognitionScript.korean);
+  ImagePicker _imagePicker = ImagePicker();
 
   @override
   void dispose() {
-
+    _canProcess = false;
+    textRecognizer.close();
     super.dispose();
   }
 
@@ -54,21 +64,121 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body:  buildGalleryView(),
-      // body: CameraView(
-      //   customPaint: _customPaint,
-      //   onImage: _processImage,
-      // ),
+     // body: buildGalleryView(),
+      body: CameraView(
+        customPaint: _customPaint,
+        onImage: _processImage,
+      ),
     );
   }
 
   Widget buildGalleryView() => Column(
-        children: [],
+        children: [
+          DropdownButton(
+              items: TextRecognitionScript.values
+                  .map(
+                    (e) => DropdownMenuItem(
+                      value: e,
+                      child: Text(e.name),
+                    ),
+                  )
+                  .toList(),
+              value: _script,
+              onChanged: (script) {
+                if (script != null) {
+                  setState(() {
+                    _script = script;
+                    textRecognizer.close();
+                    textRecognizer = TextRecognizer(script: _script);
+                  });
+                }
+              }),
+          _image != null
+              ? SizedBox(
+                  height: 320,
+                  width: 320,
+                  child: Image.file(
+                    _image!,
+                  ),
+                )
+              : Container(
+                  height: 240,
+                  width: 240,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                ),
+          ElevatedButton(
+            onPressed: () => _getImage(ImageSource.gallery),
+            child: Text("갤러리 이미지 가져오기"),
+          ),
+          Expanded(
+              child: SingleChildScrollView(
+            child: Text(
+              _text,
+            ),
+          ))
+        ],
       );
 
-  Future _getImage(ImageSource source) async {}
+  Future _getImage(ImageSource source) async {
+    setState(() {
+      _image = null;
+    });
+    final pickedFile = await _imagePicker.pickImage(source: source);
+    if (pickedFile != null) {
+      _processFile(pickedFile.path);
+    }
+  }
 
-  Future _processFile(String path) async {}
+  Future _processFile(String path) async {
+    setState(() {
+      _image = File(path);
+    });
+    final inputImage = InputImage.fromFilePath(path);
+    _processImage(inputImage);
+  }
 
-  Future<void> _processImage(InputImage inputImage) async {}
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+
+    final recognizedText = await textRecognizer.processImage(inputImage);
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+      //print(recognizedText.text);
+      final painter = TextRecognizerPainter(
+        recognizedText,
+        inputImage.metadata!.size,
+        inputImage.metadata!.rotation,
+        _cameraLensDirection,
+      );
+      _customPaint = CustomPaint(
+        painter: painter,
+      );
+    } else {
+      //갤러리에서 가져온 값 처리
+      _text = "recognized Text: \n\n";
+      for (var block in recognizedText.blocks) {
+        _text += "Block: ${block.text}\n";
+        for (var line in block.lines) {
+          _text += "Line: ${line.text}\n";
+          for (var element in line.elements) {
+            _text += "Element: ${element.text}\n";
+          }
+        }
+        _text += "\n\n";
+      }
+      setState(() {});
+      _customPaint = null;
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
 }
